e6ed254e72c2b77372582949ec0d3a4442dc48dd
Svetlased <svetla-sed2@mail.ru>
2021-04-03 11:47:39 +0200

    Firstcommit variables

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -6,22 +6,34 @@
  */
 
 function averageMark(a, g, p) {
+	// реализуйте здесь задание №1
+    let algebra = a;
+	let geography = g;
+	let physics = p;
+
+	let count = 3;
+	let sum = algebra + geography + physics;
+	let average = sum/count;
 	
-    // реализуйте здесь задание №1
-    
-	return // ваш ответ;
+	return average// ваш ответ;
 }
 
 function sayHello(userName) {
 	
     // реализуйте здесь задание №2
-    
-	return // ваш ответ;
+    let myName = userName;
+	let message = "Привет, мир! Меня зовут " + myName;
+	return message;
 }
 
 function calculateFormula() {
     
 	// реализуйте здесь задание №3
-    
-    return // ваш ответ;
+	let x = 2;
+	let y = 22;
+	let z = 0;
+
+	result = x * y + 5 * z + x - 1;
+
+    return result;
 }
