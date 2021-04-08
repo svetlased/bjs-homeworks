0ac8dffa9bb8dda2b5e6db220cef37cbf7a1f887
Svetlased <svetla-sed2@mail.ru>
2021-04-08 13:57:17 +0200

    Correction is added

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,44 +1,43 @@
-function getResult(a,b,c){
-    "use strict";
-    let d = b** + 4 * a * c;
-    let x = [];
-    
-    if ( d < 0 ) {
-        return x;
+"use strict";
+function getResult(a,b,c) {
+    let d = [b**2 - 4 * a * c];
+        
+    if ( d > 0 ) {
+        return [(- b + Math.sqrt(d)) / (a * 2), (- b - Math.sqrt(d)) / (a * 2)];
     } else if ( d === 0 ) {
-        return x = [-b/2 * a ]
+        return [((-b)/2 * a)];
     } else {
-        return x = [(- b + d) / (a * 2), (- b - d) / (a * 2)];
+        return [];
     }
 }
 
 function getAverageMark(marks){
-    let sum = 0;
-    let count = 0;
+    let sum;
+    let averageMark;
     
     if (marks === []) {
-        return 0;
+        console.log(0);
     } else if (marks.length > 5){
         console.log("Считаются только последние 5 оценок");
-        marks = marks.splice(0, 4);
+        marks = marks.splice(0, 5);
     }
-    for (let i = 0; i< marks.length; i++) {
-        count++;
+    for (let i = 0; i < marks.length; i++) {
         sum += marks[i];
-        let averageMark = sum / count
     }
-    return averageMark;
+    return averageMark = sum / marks.length;
 }
 
 function askDrink(name,dateOfBirthday){
     // код для задачи №3 писать здесь
-    let age = getFullYear(dateOfBirthday);
-    let date = newDate(dateOfBirthday);
+    let today = new Date();
+    let todayYear = today.getFullYear;
+    let age = todayYear - dateOfBirthday.getFullYear();
     let result;
-    if (age > 18) {
-        result = `Не желаете ли олд-фэшн, ${name}`;
+
+    if (age >= 18) {
+        result = `Не желаете ли олд-фэшн, ${name}?`;
     } else {
-        result = `Сожалею, ${name}`;
+        result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Могу предложить вам замечательный клюквенный компот!`;
     }
     
     return result;
