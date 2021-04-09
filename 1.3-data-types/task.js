f496bbecc39198da2ffcb2d46d2013afb061d2bd
Svetlased <svetla-sed2@mail.ru>
2021-04-10 00:25:59 +0200

    Validation is added

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,19 +1,32 @@
 "use strict";
 function calculateTotalMortgage(percent, contribution, amount, date) {
     // код для задачи №1 писать здесь
-    percent = Number(percent);
-    contribution = Number(contribution);
-    amount = Number(amount);
+    console.log(typeof percent, contribution, amount, date);
+    if (typeof percent !== 'number'){
+        return `Параметр percent содержит неправильное значение ${percent}`;
+    } else if (typeof contribution !== 'number'){
+        return `Параметр contribution содержит неправильное значение ${contribution}`;
+    } else if (typeof amount !== 'number'){
+        return `Параметр percent содержит неправильное значение ${percent}`;
+    }
+    // percent = Number(percent);
+    // contribution = Number(contribution);
+    // amount = Number(amount);
+    console.log(percent, contribution, amount, date)
 
     let today = new Date();
 
-    let monthPayment = 0;
     let bodyCredit = amount - contribution;
-    let time = (today.getFullYear() - date.getFullYear())*12 + (date.getMonth() - today.getMonth());
-    monthPayment = bodyCredit * (((1/12) * percent) + ((1/12) * percent)/(((1 + ((1/12) * percent))**time) - 1)) / 100 ;
+    let indexP = percent / (12 * 100);
+    let time = (date.getFullYear() - today.getFullYear())*12 + (date.getMonth() - today.getMonth());
+    let monthPayment = bodyCredit * (indexP + indexP/(((1 + indexP)**time) - 1));
     let totalAmount = time * monthPayment;
 
-    return totalAmount.toFixed(2);
+    totalAmount.toFixed(2);
+    totalAmount = Math.round(totalAmount * 100) / 100;
+    console.log(totalAmount);
+
+    return Number(totalAmount);
 }
 
 function getGreeting(name) {
