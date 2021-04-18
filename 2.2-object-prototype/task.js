869abfa47d0055af964f7a50fda0ef1fa4a5d016
Svetlased <svetla-sed2@mail.ru>
2021-04-18 12:09:43 +0200

    resolutions are Added

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,38 @@
-//String.prototype.isPalindrome - для задачи №1
+function isPalindrome() {
+    let result = this.split('').reverse().join('');
+    if (result.toLowerCase() === this.toLowerCase()) {
+        return true;
+    } else {
+        return false;
+    }
+}
+// или 
+// function isPalindrome() {
+// return this == this.split('').reverse().join('');
+// }
+
+String.prototype.isPalindrome = isPalindrome;
+
 
 function getAverageMark(marks) {
     // код для задачи №2 писать здесь
-    // return averageMark
+
+    if (marks.length === 0) {
+        return 0;
+    }
+    let sum = 0;
+    for (let i = 0; i < marks.length; i++) {
+        sum += marks[i];
+    }
+    return Math.round(sum / marks.length);
 }
 
 function checkBirthday(birthday) {
     // код для задачи №3 писать здесь
-    // return verdict
+    const now = new Date();
+    const data = new Date(birthday.split('-'));
+    birthday = Date([Number(birthday[0]), Number((+birthday[1])-1), Number(+birthday[2])]);
+    let diff = birthday - now;
+    let age = diff / 31536000000;
+    return age >= 18;
 }
