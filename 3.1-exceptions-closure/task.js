f3419e463cab73bc0dd64d5d1683de3009696b78
Svetlased <svetla-sed2@mail.ru>
2021-04-26 14:27:25 +0200

    corrections are added

--- 3.1-exceptions-closure/task.js
+++ 3.1-exceptions-closure/task.js
@@ -12,30 +12,35 @@ function validateCount(valueForPars) {
     }
     return parseCount(valueForPars);
 }
-
 class Triangle {
     constructor(a, b, c) {
-        if (a + b < c && a + c < b && b + c < a) {
+        if (!(a + b > c && a + c > b && b + c > a)) {
             throw new Error("Треугольник с такими сторонами не существует");
         }
-        // this.a = a;
-        // this.b = b;
-        // this.c = c;
+        this.a = a;
+        this.b = b;
+        this.c = c; 
     }
     getPerimeter() {
-        return a + b + c;
+        return this.a + this.b + this.c;
     }
     getArea(){
-        this.semiPerim = this.getPerimeter() * 0.5;
-        let square = Math.sqrt(this.semiPerim * (this.semiPerim - this.a) * (this.semiPerim - this.b) * (this.semiPerim - this.c));
-        return Number.square.toFixed(3);
+        let semiPerim = this.getPerimeter() * 0.5;
+        return Number(Math.sqrt(semiPerim * (semiPerim - this.a) * (semiPerim - this.b) * (semiPerim - this.c)).toFixed(3));
     }
 }
 function getTriangle(a, b, c) {
     try {
-        return new Triangle;
-    } catch {
-        return {getArea, getPerimeter}
+        return new Triangle(a, b, c);
+    } catch(err) {
+        return {
+        getArea() {
+            return 'Ошибка! Треугольник не существует';
+          },
+          getPerimeter() {
+            return 'Ошибка! Треугольник не существует';
+          }
+        }
     }
-
 }
+    
